be45ff1b716a0bbad3ec1377fab7c0b5e8d0a488
amitsha9634 <amitsha9634@gmail.com>
2024-07-26 04:59:33 +0000

    dataTypes.js

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS as newer version
+
+// alert (3 + 3) // we are using node.js, not browser 
+
+console.log(3 
+    +
+    3); // code readability should be high
+
+    console.log("Amit");
+
+    let = "Amit";
+    let age = 19;
+    let isAdult = true;
+
+    // number => 2 to poer 53 
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof undefined); // undefineed
+    console.log(typeof null); // object
