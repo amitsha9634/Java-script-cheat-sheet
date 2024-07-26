67ae5a413a7f912550fa929ffcdfd99b9111c490
amitsha9634 <amitsha9634@gmail.com>
2024-07-26 05:30:14 +0000

    Conversiof of datatypes

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined;
+
+/* console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "amit";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// "" => false 
+// "XYZ" => true
+
+let someNumber = 33;
+
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
