393dc561c1122f19d56598a68c27cbff0adda7c5
amitsha9634 <amitsha9634@gmail.com>
2024-07-27 11:00:36 +0000

    06_nums_and_math.js

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 500
+//console.log(score);
+
+const balance = new Number(300);
+//console.log(balance);
+
+
+console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const otherNumber = 25.5555
+//console.log(otherNumber.toPrecision(2));
+
+const hundreads = 7000000
+//console.log(hundreads.toLocaleString('en-IN'));
+
+
+//*********************** Maths ***********************
+
+
+console.log(Math);
+/*console.log(Math.abs(-5));
+console.log(Math.ceil(5.5));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.4));*/
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
