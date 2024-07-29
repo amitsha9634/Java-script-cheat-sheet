8eb342dbf20812767c8b962098814e9f8ce55927
Amit Sharma <amitsha9634@gmail.com>
2024-07-29 11:13:24 +0000

    02_scopes.js

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,47 @@
+// var c = 300
+
+let a = 300
+
+if (true){
+   let a = 30
+   let b = 50
+   //console.log("INNER:", a)
+}
+
+// console.log(a);
+
+
+
+
+function one (){
+   const username = "Amit"
+
+   function two(){
+   const website = "Youtube"
+   console:log(username);
+   }
+
+   // console.log(website);
+
+   two()
+}
+
+// one()
+
+
+if(true){
+   const username = "Amit"
+
+   if(username === "Amit"){
+      const website = "youtube"
+      console.log(username + website);
+   }
+   //console.log(website)
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++ interesting +++++++++++++++++++++
+
+function 
+
