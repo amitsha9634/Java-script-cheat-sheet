0486ffdb2fb58113e25b115ffb351ac88edb03a4
Amit Sharma <amitsha9634@gmail.com>
2024-07-30 11:42:41 +0000

    four.js

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const language = {
+   js : 'Javascript', 
+   cpp: 'C++',
+   rb: 'ruby',
+   swift: 'swift by apple'
+}
+
+for (const key in language) {
+  // console.log(`${key} shortcut is for ${language[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "ruby"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
